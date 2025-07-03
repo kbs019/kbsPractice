f7196c1b333b3f0a76508847363c98f2dd423b20
kbs019 <qjatn019@gmail.com>
2025-07-03 13:13:00 +0900

    ex12 로 변경

--- src/main/java/com/ex/controller/ParamController.java
+++ src/main/java/com/ex/controller/ParamController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ParamController {
     
-    @GetMapping("ex01")
-    public String ex01(){
-        return "ex01";
+    @GetMapping("ex012")
+    public String ex012(){
+        return "ex012";
     }
 }
